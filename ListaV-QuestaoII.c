327ffcc533b401b7ce31571a97623c28b31d9ad1
ViniciusRoquef <71899135+ViniciusRoquef@users.noreply.github.com>
2020-10-20 23:57:59 -0300

    Create ListaV-QuestaoII.c

---
+++ ListaV-QuestaoII.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+int main () {
+    struct hr {
+        int hr, min, seg;
+    };
+
+    struct data {
+        int dia, mes, ano;
+    };
+
+    struct.afazer {
+        struct data esta_data;
+        struct horario este_hr;
+        char afazer [100];
+    };
+
+    return 0;
+}
